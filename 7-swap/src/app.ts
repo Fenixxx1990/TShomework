e04f0b1cd62e9e305226ed7f48afbd6b1d68b04d
Fenix <fenixxx1990@yandex.ru>
2026-02-25 01:17:14 +0400

    finish

--- 7-swap/src/app.ts
+++ 7-swap/src/app.ts
@@ -1,3 +1,22 @@
-interface iLogLine<T> {
-  data: T;
+function swapKeysAndValues<T extends Record<string, number>>(
+  obj: T,
+): Record<number, string> {
+  const result: Record<number, string> = {};
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const value = obj[key];
+      result[value as number] = key;
+    }
+  }
+
+  return result;
 }
+
+// С числовыми значениями
+const numbers = { x: 10, y: 20 };
+console.log(swapKeysAndValues(numbers)); // { '10': 'x', '20': 'y' }
+
+// Пустой объект
+const empty = {};
+console.log(swapKeysAndValues(empty)); // {}
