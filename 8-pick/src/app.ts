55e8a88866f8066b0cec3d60ad5dc5a2c51233ef
Fenix <fenixxx1990@yandex.ru>
2026-02-26 11:52:00 +0400

    finish

--- 8-pick/src/app.ts
+++ 8-pick/src/app.ts
@@ -4,6 +4,31 @@ const user = {
   skills: ["typescript", "javascript"],
 };
 
+interface User {
+  name: string;
+  age: number;
+  skills: string[];
+}
+
+function pickObjectKeys<T extends User, K extends keyof T>(
+  obj: T,
+  keys: K[],
+): Pick<T, K> {
+  const result = {} as Pick<T, K>;
+
+  for (const key of keys) {
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
+}
+
+console.log(pickObjectKeys(user, ["name", "age"])); // { name: "Vasiliy", age: 8 }
+console.log(pickObjectKeys(user, ["name", "skills"])); // { name: "Vasiliy", skills: ["typescript", "javascript"] }
+console.log(pickObjectKeys(user, ["age", "skills"])); // { age: 8, skills: ["typescript", "javascript"] }
+
 // const res = pickObjectKeys(user, ["age", "skills"]);
 /*
 {
