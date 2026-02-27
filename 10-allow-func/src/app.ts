714886c278daa1564ec9819ebb053d45b318d2b6
Fenix <fenixxx1990@yandex.ru>
2026-02-27 21:23:50 +0400

    finish

--- 10-allow-func/src/app.ts
+++ 10-allow-func/src/app.ts
@@ -1,33 +1,42 @@
-interface IA {
-  a: number;
-  b: string;
+function allowFunc(validator: (value: any) => boolean) {
+  return function (target: any, propertyKey: string) {
+    // Создаём приватное поле для хранения значения
+    const privateField = `_${propertyKey}`;
+
+    // Определяем свойство с геттером и сеттером
+    Object.defineProperty(target, propertyKey, {
+      // Геттер: возвращает текущее значение
+      get: function () {
+        return this[privateField];
+      },
+
+      // Сеттер: проверяет значение через валидатор перед присваиванием
+      set: function (value: any) {
+        if (validator(value)) {
+          this[privateField] = value;
+        }
+        // Если валидация не пройдена, значение не меняется
+      },
+
+      // Делаем свойство перечисляемым (будет видно в Object.keys и т. д.)
+      enumerable: true,
+
+      // Разрешаем переопределение свойства через Object.defineProperty
+      configurable: true,
+    });
+  };
 }
 
-interface IB {
-  a: number;
-  c: boolean;
+class User {
+  @allowFunc((a: number) => a > 0)
+  age: number = 30;
 }
 
-let a: IA = { a: 5, b: "" };
-let b: IB = { a: 10, c: true };
+const person = new User();
+console.log(person.age); // 30
 
-interface IDifference {
-  b: string;
-}
+person.age = 0;
+console.log(person.age); // 30
 
-function difference<T extends object, U extends object>(
-  obj1: T,
-  obj2: U,
-): Omit<T, keyof U> {
-  const result = { ...obj1 };
-
-  (Object.keys(obj2) as (keyof U)[]).forEach((key) => {
-    if (key in result) {
-      delete (result as any)[key];
-    }
-  });
-
-  return result as Omit<T, keyof U>;
-}
-
-let v0: IDifference = difference(a, b);
+person.age = 20;
+console.log(person.age); // 20
